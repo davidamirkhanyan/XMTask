e3dbd099003d3c10873963cb8752d7cb298b8f67
Davit Amirkhanyan <davit.amirkhanyan@nork.am>
2023-05-09 23:58:09 +0400

    conftest.py update

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
@@ -9,7 +10,7 @@ def pytest_addoption(parser):
     parser.addoption("--env", action="store", default="Prod")
     parser.addoption("--browser", action="store", default="Chrome")
     parser.addoption("--headless", action="store", default="yes")
-    
+
 
 def pytest_configure(config):
     if config.getoption('env') == "Prod":
@@ -19,23 +20,23 @@ def pytest_configure(config):
     os.environ['url'] = url
     os.environ['browser'] = config.getoption('browser')
     os.environ["headless"] = config.getoption('headless')
-    
+
 
 @pytest.fixture(scope="class")
 def set_up(request):
+    driver = None
     if os.environ['browser'] == "Chrome":
         options = webdriver.ChromeOptions()
         if os.environ["headless"] == 'yes':
             options.add_argument("--headless")
-        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     elif os.environ['browser'] == "Firefox":
         options = webdriver.FirefoxOptions()
         if os.environ["headless"] == 'yes':
             options.headless = True
-        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
+        driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
     driver.maximize_window()
     driver.get(os.environ['url'])
     request.cls.driver = driver
     yield driver
     driver.quit()
-    
